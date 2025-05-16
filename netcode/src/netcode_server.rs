4a7a6c928ba81060c37a7e464672ae9282838850
rejdeboer <rick.deboer@live.nl>
2025-05-16 19:07:35 +0200

    feat: copy server transport code

--- netcode/src/netcode_server.rs
+++ netcode/src/netcode_server.rs
@@ -1,5 +1,9 @@
+use std::net::UdpSocket;
 use std::{collections::HashMap, net::SocketAddr, time::Duration};
 
+use crate::ClientId;
+use crate::error::NetcodeTransportError;
+use crate::server::RenetServer;
 use crate::{
     CONNECT_TOKEN_PRIVATE_BYTES, CONNECT_TOKEN_XNONCE_BYTES, KEY_BYTES, MAC_BYTES, MAX_CLIENTS,
     MAX_PACKET_BYTES, MAX_PAYLOAD_BYTES, MAX_PENDING_CLIENTS, SEND_RATE, USER_DATA_BYTES,
@@ -811,6 +815,185 @@ fn find_client_mut_by_addr(
         Some(c) if c.addr == addr => Some((i, c)),
         _ => None,
     })
+}
+
+#[derive(Debug)]
+pub struct NetcodeServerTransport {
+    socket: UdpSocket,
+    netcode_server: NetcodeServer,
+    buffer: [u8; MAX_PACKET_BYTES],
+}
+
+impl NetcodeServerTransport {
+    pub fn new(server_config: ServerConfig, socket: UdpSocket) -> Result<Self, std::io::Error> {
+        socket.set_nonblocking(true)?;
+
+        let netcode_server = NetcodeServer::new(server_config);
+
+        Ok(Self {
+            socket,
+            netcode_server,
+            buffer: [0; MAX_PACKET_BYTES],
+        })
+    }
+
+    /// Returns the server public address
+    pub fn addresses(&self) -> Vec<SocketAddr> {
+        self.netcode_server.addresses()
+    }
+
+    /// Returns the maximum number of clients that can be connected.
+    pub fn max_clients(&self) -> usize {
+        self.netcode_server.max_clients()
+    }
+
+    /// Update the maximum numbers of clients that can be connected.
+    ///
+    /// Changing the `max_clients` to a lower value than the current number of connect clients
+    /// does not disconnect clients. So [`NetcodeServerTransport::connected_clients()`] can
+    /// return a higher value than [`NetcodeServerTransport::max_clients()`].
+    pub fn set_max_clients(&mut self, max_clients: usize) {
+        self.netcode_server.set_max_clients(max_clients);
+    }
+
+    /// Returns current number of clients connected.
+    pub fn connected_clients(&self) -> usize {
+        self.netcode_server.connected_clients()
+    }
+
+    /// Returns the user data for client if connected.
+    pub fn user_data(&self, client_id: ClientId) -> Option<[u8; USER_DATA_BYTES]> {
+        self.netcode_server.user_data(client_id)
+    }
+
+    /// Returns the client address if connected.
+    pub fn client_addr(&self, client_id: ClientId) -> Option<SocketAddr> {
+        self.netcode_server.client_addr(client_id)
+    }
+
+    /// Disconnects all connected clients.
+    /// This sends the disconnect packet instantly, use this when closing/exiting games,
+    /// should use [RenetServer::disconnect_all][crate::RenetServer::disconnect_all] otherwise.
+    pub fn disconnect_all(&mut self, server: &mut RenetServer) {
+        for client_id in self.netcode_server.clients_id() {
+            let server_result = self.netcode_server.disconnect(client_id);
+            handle_server_result(server_result, &self.socket, server);
+        }
+    }
+
+    /// Returns the duration since the connected client last received a packet.
+    /// Usefull to detect users that are timing out.
+    pub fn time_since_last_received_packet(&self, client_id: ClientId) -> Option<Duration> {
+        self.netcode_server
+            .time_since_last_received_packet(client_id)
+    }
+
+    /// Advances the transport by the duration, and receive packets from the network.
+    pub fn update(
+        &mut self,
+        duration: Duration,
+        server: &mut RenetServer,
+    ) -> Result<(), NetcodeTransportError> {
+        self.netcode_server.update(duration);
+
+        loop {
+            match self.socket.recv_from(&mut self.buffer) {
+                Ok((len, addr)) => {
+                    let server_result = self
+                        .netcode_server
+                        .process_packet(addr, &mut self.buffer[..len]);
+                    handle_server_result(server_result, &self.socket, server);
+                }
+                Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => break,
+                Err(ref e) if e.kind() == std::io::ErrorKind::Interrupted => break,
+                Err(ref e) if e.kind() == std::io::ErrorKind::ConnectionReset => continue,
+                Err(e) => return Err(e.into()),
+            };
+        }
+
+        for client_id in self.netcode_server.clients_id() {
+            let server_result = self.netcode_server.update_client(client_id);
+            handle_server_result(server_result, &self.socket, server);
+        }
+
+        for disconnection_id in server.disconnections_id() {
+            let server_result = self.netcode_server.disconnect(disconnection_id);
+            handle_server_result(server_result, &self.socket, server);
+        }
+
+        Ok(())
+    }
+
+    /// Send packets to connected clients.
+    pub fn send_packets(&mut self, server: &mut RenetServer) {
+        'clients: for client_id in server.clients_id() {
+            let packets = server.get_packets_to_send(client_id).unwrap();
+            for packet in packets {
+                match self
+                    .netcode_server
+                    .generate_payload_packet(client_id, &packet)
+                {
+                    Ok((addr, payload)) => {
+                        if let Err(e) = self.socket.send_to(payload, addr) {
+                            tracing::error!(
+                                "Failed to send packet to client {client_id} ({addr}): {e}"
+                            );
+                            continue 'clients;
+                        }
+                    }
+                    Err(e) => {
+                        tracing::error!(
+                            "Failed to encrypt payload packet for client {client_id}: {e}"
+                        );
+                        continue 'clients;
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn handle_server_result(
+    server_result: ServerResult,
+    socket: &UdpSocket,
+    reliable_server: &mut RenetServer,
+) {
+    let send_packet = |packet: &[u8], addr: SocketAddr| {
+        if let Err(err) = socket.send_to(packet, addr) {
+            tracing::error!("Failed to send packet to {addr}: {err}");
+        }
+    };
+
+    match server_result {
+        ServerResult::None => {}
+        ServerResult::PacketToSend { payload, addr } => {
+            send_packet(payload, addr);
+        }
+        ServerResult::Payload { client_id, payload } => {
+            if let Err(e) = reliable_server.process_packet_from(payload, client_id) {
+                tracing::error!("Error while processing payload for {}: {}", client_id, e);
+            }
+        }
+        ServerResult::ClientConnected {
+            client_id,
+            user_data: _,
+            addr,
+            payload,
+        } => {
+            reliable_server.add_connection(client_id);
+            send_packet(payload, addr);
+        }
+        ServerResult::ClientDisconnected {
+            client_id,
+            addr,
+            payload,
+        } => {
+            reliable_server.remove_connection(client_id);
+            if let Some(payload) = payload {
+                send_packet(payload, addr);
+            }
+        }
+    }
 }
 
 #[cfg(test)]
