f003b165abc6376ae0c095e963c5432c1edd984b
rejdeboer <rick.deboer@live.nl>
2025-05-19 12:16:38 +0200

    feat: characters table

---
+++ migrations/20250519085619_create_characters.sql
@@ -0,0 +1,32 @@
+DROP TABLE users;
+CREATE TABLE IF NOT EXISTS accounts (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    passhash VARCHAR(50) NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS "idx_accounts_id" ON "accounts" ("id");
+
+CREATE TABLE IF NOT EXISTS characters (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    account_id INT NOT NULL REFERENCES accounts(id)
+        ON DELETE CASCADE,
+
+    level INT NOT NULL DEFAULT 1,
+    experience BIGINT NOT NULL DEFAULT 0,
+
+    position_x DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    position_y DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    position_z DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    rotation_yaw DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+
+    is_online BOOLEAN NOT NULL DEFAULT FALSE,
+
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX IF NOT EXISTS "idx_characters_id" ON "characters" ("id");
+CREATE INDEX IF NOT EXISTS idx_characters_account_id ON characters(account_id);
