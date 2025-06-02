d6ad4361454ea515a5ef36713b75185c5ed31281
rejdeboer <rick.deboer@live.nl>
2025-06-02 16:55:03 +0200

    fix: db init script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -44,7 +44,7 @@ done
 >&2 echo "Postgres is up and running on port ${DB_PORT}!"
 
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
-sqlx database create
-sqlx migrate run 
+cargo sqlx database create
+cargo sqlx migrate run 
 
 >&2 echo "Postgres has been migrated, ready to go!"
