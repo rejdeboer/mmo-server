1fb7efb09be292835e4cf5dab8d2cb57de4ff7ff
rejdeboer <rick.deboer@live.nl>
2025-06-03 08:24:55 +0200

    feat: correct flatbuffer compilation

--- schemas/build.rs
+++ schemas/build.rs
@@ -2,24 +2,28 @@ use std::path::Path;
 
 fn main() {
     let schema_files = [
-        "schemas/character.fbs",
         "schemas/common.fbs",
-        "schemas/enter_game_request.fbs",
-        "schemas/enter_game_response.fbs",
         "schemas/entity.fbs",
         "schemas/event.fbs",
+        "schemas/character.fbs",
+        "schemas/enter_game_request.fbs",
+        "schemas/enter_game_response.fbs",
     ];
 
     for schema in &schema_files {
         println!("cargo:rerun-if-changed={}", schema);
     }
 
-    for schema in &schema_files {
-        flatc_rust::run(flatc_rust::Args {
-            inputs: &[Path::new(schema)],
-            out_dir: Path::new("src/"),
-            ..Default::default()
-        })
-        .expect(&format!("Failed to compile {}", schema));
-    }
+    let paths: Vec<&Path> = schema_files
+        .into_iter()
+        .map(|schema| Path::new(schema))
+        .collect();
+
+    flatc_rust::run(flatc_rust::Args {
+        inputs: &paths,
+        out_dir: Path::new("src/"),
+        extra: &["--rust-module-root-file"],
+        ..Default::default()
+    })
+    .expect(&format!("Failed to compile"));
 }
