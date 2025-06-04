5d248cfe74935d26cbe550ec4a09601e79173843
rejdeboer <rick.deboer@live.nl>
2025-06-04 11:17:15 +0200

    feat: simple renet client connection code

---
+++ mmo-client/src/client.rs
@@ -0,0 +1,34 @@
+use std::net::{IpAddr, SocketAddr, UdpSocket};
+use std::time::SystemTime;
+
+use renet::{ConnectionConfig, RenetClient};
+use renet_netcode::{ClientAuthentication, NetcodeClientTransport};
+
+pub struct GameClient {
+    client: RenetClient,
+    transport: NetcodeClientTransport,
+}
+
+impl GameClient {
+    pub fn connect(host: String, port: u16) -> Self {
+        let client = RenetClient::new(ConnectionConfig::default());
+        let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
+
+        let ip_addr = IpAddr::V4(host.parse().expect("host should be IPV4 addr"));
+        let server_addr: SocketAddr = SocketAddr::new(ip_addr, port);
+
+        let current_time = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap();
+        let authentication = ClientAuthentication::Unsecure {
+            server_addr,
+            client_id: 0,
+            user_data: None,
+            protocol_id: 0,
+        };
+
+        let transport = NetcodeClientTransport::new(current_time, authentication, socket).unwrap();
+
+        Self { client, transport }
+    }
+}
