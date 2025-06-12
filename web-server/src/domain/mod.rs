fd298e1b10162d9630f47b79834baadea2d5394f
rejdeboer <rick.deboer@live.nl>
2025-06-12 17:43:22 +0200

    feat: basic password validation

--- web-server/src/domain/mod.rs
+++ web-server/src/domain/mod.rs
@@ -1,5 +1,7 @@
 mod email_address;
+mod password;
 mod username;
 
 pub use email_address::*;
+pub use password::*;
 pub use username::*;
--- web-server/src/domain/password.rs
+++ web-server/src/domain/password.rs
@@ -1,25 +1,25 @@
 #[derive(Debug)]
-pub struct Username(String);
+pub struct Password(String);
 
-impl Username {
+impl Password {
     // TODO: Verify if we use normal constraints
-    pub fn parse(s: String) -> Result<Username, String> {
+    pub fn parse(s: String) -> Result<Password, String> {
         let is_empty_or_whitespace = s.trim().is_empty();
         let is_too_long = s.len() > 32;
-        let is_ascii = s.is_ascii();
+        let is_too_short = s.len() < 8;
 
-        let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}', ' '];
+        let forbidden_characters = ['(', ')', '"', '<', '>', '\\', '{', '}', ' '];
         let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
 
-        if is_empty_or_whitespace || is_ascii || is_too_long || contains_forbidden_characters {
-            Err(format!("{} is not a valid username", s))
+        if is_empty_or_whitespace || is_too_long || is_too_short || contains_forbidden_characters {
+            Err(format!("{} is not a valid password", s))
         } else {
             Ok(Self(s.to_lowercase()))
         }
     }
 }
 
-impl AsRef<str> for Username {
+impl AsRef<str> for Password {
     fn as_ref(&self) -> &str {
         &self.0
     }
@@ -31,19 +31,19 @@ mod tests {
     use claim::{assert_err, assert_ok};
 
     #[test]
-    fn a_32_char_long_name_is_valid() {
+    fn a_32_char_long_pw_is_valid() {
         let name = "a".repeat(32);
         assert_ok!(Password::parse(name));
     }
 
     #[test]
-    fn a_name_longer_than_32_chars_is_rejected() {
+    fn a_pw_longer_than_32_chars_is_rejected() {
         let name = "a".repeat(33);
         assert_err!(Password::parse(name));
     }
 
     #[test]
-    fn whitespace_only_names_are_rejected() {
+    fn whitespace_only_pws_are_rejected() {
         let name = " ".to_string();
         assert_err!(Password::parse(name));
     }
@@ -55,16 +55,16 @@ mod tests {
     }
 
     #[test]
-    fn names_containing_an_invalid_character_are_rejected() {
-        for name in &['/', '(', ')', '"', '<', '>', '\\', '{', '}', ' '] {
+    fn pws_containing_an_invalid_character_are_rejected() {
+        for name in &['(', ')', '"', '<', '>', '\\', '{', '}', ' '] {
             let name = name.to_string();
             assert_err!(Password::parse(name));
         }
     }
 
     #[test]
-    fn a_valid_name_is_pared_successfully() {
-        let name = "rejdeboer".to_string();
+    fn a_valid_pw_is_pared_successfully() {
+        let name = "superSecret@".to_string();
         assert_ok!(Password::parse(name));
     }
 }
