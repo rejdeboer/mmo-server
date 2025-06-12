944bc40c9583c737fe928cb1ca2554c519704990
rejdeboer <rick.deboer@live.nl>
2025-06-12 21:25:22 +0200

    feat: character name validation

---
+++ web-server/src/domain/character_name.rs
@@ -0,0 +1,69 @@
+#[derive(Debug)]
+pub struct CharacterName(String);
+
+impl CharacterName {
+    // TODO: Verify if we use normal constraints
+    pub fn parse(s: String) -> Result<CharacterName, String> {
+        let is_too_long = s.len() > 32;
+        let is_too_short = s.len() < 3;
+
+        let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}', ' '];
+        let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
+
+        if is_too_long || is_too_short || contains_forbidden_characters {
+            Err(format!("{} is not a valid username", s))
+        } else {
+            Ok(Self(s.to_lowercase()))
+        }
+    }
+}
+
+impl AsRef<str> for CharacterName {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::domain::Password;
+    use claim::{assert_err, assert_ok};
+
+    #[test]
+    fn a_32_char_long_name_is_valid() {
+        let name = "a".repeat(32);
+        assert_ok!(Password::parse(name));
+    }
+
+    #[test]
+    fn a_name_longer_than_32_chars_is_rejected() {
+        let name = "a".repeat(33);
+        assert_err!(Password::parse(name));
+    }
+
+    #[test]
+    fn whitespace_only_names_are_rejected() {
+        let name = " ".to_string();
+        assert_err!(Password::parse(name));
+    }
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let name = "".to_string();
+        assert_err!(Password::parse(name));
+    }
+
+    #[test]
+    fn names_containing_an_invalid_character_are_rejected() {
+        for name in &['/', '(', ')', '"', '<', '>', '\\', '{', '}', ' '] {
+            let name = name.to_string();
+            assert_err!(Password::parse(name));
+        }
+    }
+
+    #[test]
+    fn a_valid_name_is_pared_successfully() {
+        let name = "rejdeboer".to_string();
+        assert_ok!(Password::parse(name));
+    }
+}
--- web-server/src/domain/mod.rs
+++ web-server/src/domain/mod.rs
@@ -1,7 +1,9 @@
+mod character_name;
 mod email_address;
 mod password;
 mod username;
 
+pub use character_name::*;
 pub use email_address::*;
 pub use password::*;
 pub use username::*;
