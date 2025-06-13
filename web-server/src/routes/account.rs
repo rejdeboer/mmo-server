252ea6cbf82cc93a9b996448fe311e7ce9b8e768
rejdeboer <rick.deboer@live.nl>
2025-06-13 08:55:23 +0200

    feat: start working on account route

--- web-server/src/routes/account.rs
+++ web-server/src/routes/account.rs
@@ -1,21 +1,38 @@
-use crate::{auth::User, error::ApiError, ApplicationState};
-use axum::extract::ConnectInfo;
-use axum::{extract::State, response::Response, Extension};
-use axum_extra::TypedHeader;
-use std::net::SocketAddr;
+use crate::domain::{EmailAddress, Password, Username};
+use crate::{error::ApiError, ApplicationState};
+use axum::Json;
+use axum::{extract::State, response::Response};
 
-// TODO: Add connection context for tracing
-pub async fn account_post(
-    user_agent: Option<TypedHeader<headers::UserAgent>>,
-    ConnectInfo(_addr): ConnectInfo<SocketAddr>,
+pub struct AccountCreate {
+    pub username: String,
+    pub email: String,
+    pub password: String,
+}
+
+pub struct NewAccount {
+    pub username: Username,
+    pub email: EmailAddress,
+    pub password: Password,
+}
+
+impl TryInto<NewAccount> for AccountCreate {
+    type Error = String;
+
+    fn try_into(self) -> Result<NewAccount, Self::Error> {
+        let username = Username::parse(self.username)?;
+        let email = EmailAddress::parse(self.email)?;
+        let password = Password::parse(self.password)?;
+        Ok(NewAccount {
+            username,
+            email,
+            password,
+        })
+    }
+}
+
+pub async fn account_create(
     State(state): State<ApplicationState>,
-    Extension(user): Extension<User>,
+    Json(payload): Json<AccountCreate>,
 ) -> Result<Response, ApiError> {
-    let _user_agent = if let Some(TypedHeader(user_agent)) = user_agent {
-        user_agent.to_string()
-    } else {
-        String::from("Unknown client")
-    };
-
-    Ok(Response::builder().status(200).body(()).unwrap())
+    let new_account = payload.try_into().map_err(ApiError::BadRequest)?;
 }
