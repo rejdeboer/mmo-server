7228c1bec2d3be82c9d49d9d06607d921b4c7881
rejdeboer <rick.deboer@live.nl>
2025-06-14 16:02:54 +0200

    feat: extra login tests

--- web-server/tests/api/token.rs
+++ web-server/tests/api/token.rs
@@ -5,17 +5,38 @@ use crate::helpers::{spawn_app, TestApp};
 #[tokio::test]
 async fn login_success() {
     let app = spawn_app().await;
-    let response = login(&app).await;
+    let response = login(
+        &app,
+        app.account.email.clone(),
+        app.account.password.clone(),
+    )
+    .await;
     assert_eq!(response.status().as_u16(), 200);
 }
 
-async fn login(app: &TestApp) -> reqwest::Response {
+#[tokio::test]
+async fn login_wrong_password() {
+    let app = spawn_app().await;
+    let response = login(&app, app.account.email.clone(), "wrong".to_string()).await;
+    assert_eq!(response.status().as_u16(), 401);
+}
+
+#[tokio::test]
+async fn login_wrong_email() {
+    let app = spawn_app().await;
+    let response = login(
+        &app,
+        "wrong@wrong.wrong".to_string(),
+        app.account.password.clone(),
+    )
+    .await;
+    assert_eq!(response.status().as_u16(), 401);
+}
+
+async fn login(app: &TestApp, email: String, password: String) -> reqwest::Response {
     reqwest::Client::new()
         .post(format!("{}/token", app.address))
-        .json(&LoginBody {
-            email: app.account.email.clone(),
-            password: app.account.password.clone(),
-        })
+        .json(&LoginBody { email, password })
         .send()
         .await
         .expect("Failed to execute request.")
