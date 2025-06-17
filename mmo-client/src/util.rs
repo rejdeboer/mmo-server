f73409fbd2bf6368fa8e8077cdcfb87ab7e398b6
rejdeboer <rick.deboer@live.nl>
2025-06-17 22:47:51 +0200

    feat: extra space for padding

--- mmo-client/src/util.rs
+++ mmo-client/src/util.rs
@@ -2,7 +2,7 @@ use renet_netcode::{ConnectToken, NetcodeError};
 
 pub fn decode_token(encoded: String) -> Result<ConnectToken, NetcodeError> {
     // NOTE: Take the base64 padding into account
-    let mut decoded: [u8; 1100] = [0; 1100];
+    let mut decoded: [u8; 2048] = [0; 2048];
     let bytes_written = base64::decode_config_slice(encoded, base64::STANDARD, &mut decoded)
         .map_err(|_| NetcodeError::PayloadAboveLimit)?;
 
