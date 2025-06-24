d4fbc988d3083a342f45f99fbf5ff1164f185042
rejdeboer <rick.deboer@live.nl>
2025-06-24 08:05:01 +0200

    fix: schema compilation

--- schemas/src/lib.rs
+++ schemas/src/lib.rs
@@ -1,5 +1,7 @@
 pub mod mmo {
     use super::*;
+    mod vec_2_generated;
+    pub use self::vec_2_generated::*;
     mod vec_3_generated;
     pub use self::vec_3_generated::*;
     mod transform_generated;
@@ -12,6 +14,10 @@ pub mod mmo {
     pub use self::enter_game_response_generated::*;
     mod netcode_token_user_data_generated;
     pub use self::netcode_token_user_data_generated::*;
+    mod entity_move_event_generated;
+    pub use entity_move_event_generated::*;
+    mod event_data_generated;
+    pub use event_data_generated::*;
     mod event_generated;
     pub use event_generated::*;
 } // mmo
