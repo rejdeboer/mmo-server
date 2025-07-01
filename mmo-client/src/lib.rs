67ff96089d76180434b70994f5fecb14a534dff7
rejdeboer <rick.deboer@live.nl>
2025-07-01 15:31:59 +0200

    feat: export game event

--- mmo-client/src/lib.rs
+++ mmo-client/src/lib.rs
@@ -4,7 +4,7 @@ mod types;
 mod util;
 
 pub use action::PlayerAction;
-pub use client::{ConnectionEvent, GameClient};
+pub use client::{ConnectionEvent, GameClient, GameEvent};
 pub use renet_netcode::{ConnectToken, NetcodeError};
 pub use types::{Character, Transform, Vec3};
 pub use util::decode_token;
