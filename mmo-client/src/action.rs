dd70cc527c2194e5361014aa639c6154fa8b7199
rejdeboer <rick.deboer@live.nl>
2025-07-15 08:23:23 +0200

    feat: encode client chat message

--- mmo-client/src/action.rs
+++ mmo-client/src/action.rs
@@ -1,8 +1,10 @@
 use flatbuffers::{FlatBufferBuilder, WIPOffset};
+use schemas::mmo::ChannelType;
 
 use crate::Vec3;
 
 pub enum PlayerAction {
+    Chat(ChannelType, String),
     Move(Vec3, f32),
 }
 
@@ -12,6 +14,24 @@ impl PlayerAction {
         builder: &mut FlatBufferBuilder<'a>,
     ) -> WIPOffset<schemas::mmo::Action<'a>> {
         match self {
+            Self::Chat(channel, msg) => {
+                let fb_msg = builder.create_string(msg);
+                let action_data = schemas::mmo::ClientChatMessage::create(
+                    builder,
+                    &schemas::mmo::ClientChatMessageArgs {
+                        channel: *channel,
+                        target_user: None,
+                        text: Some(fb_msg),
+                    },
+                );
+                schemas::mmo::Action::create(
+                    builder,
+                    &schemas::mmo::ActionArgs {
+                        data_type: schemas::mmo::ActionData::mmo_ClientChatMessage,
+                        data: Some(action_data.as_union_value()),
+                    },
+                )
+            }
             Self::Move(pos, yaw) => {
                 let transform = schemas::mmo::Transform::new(
                     &schemas::mmo::Vec3::new(pos.x, pos.y, pos.z),
