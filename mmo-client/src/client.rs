e200db7f4020f924fbd88b56590aac3be48757b8
rejdeboer <rick.deboer@live.nl>
2025-07-22 07:34:04 +0200

    chore: code cleanup

--- mmo-client/src/client.rs
+++ mmo-client/src/client.rs
@@ -230,58 +230,58 @@ impl GameClient {
 
 fn read_event_batch(events: &mut Vec<GameEvent>, bytes: Bytes) -> Result<(), InvalidFlatbuffer> {
     let batch = root::<schemas::mmo::BatchedEvents>(&bytes)?;
-    if let Some(fb_events) = batch.events() {
-        for event in fb_events {
-            match event.data_type() {
-                schemas::mmo::EventData::mmo_ServerChatMessage => {
-                    let fb_event = event
-                        .data_as_mmo_server_chat_message()
-                        .expect("event should be some");
-                    events.push(GameEvent::Chat {
-                        channel: fb_event.channel(),
-                        author_name: fb_event
-                            .author_name()
-                            .expect("author should exist")
-                            .to_string(),
-                        text: fb_event.text().to_string(),
-                    })
-                }
-                schemas::mmo::EventData::EntityMoveEvent => {
-                    let fb_event = event
-                        .data_as_entity_move_event()
-                        .expect("event should be some");
-                    let transform = fb_event.transform().expect("transform should be some");
-                    let pos = transform.position();
-                    events.push(GameEvent::MoveEntity {
-                        entity_id: fb_event.entity_id(),
-                        transform: Transform {
-                            position: Vec3::new(pos.x(), pos.y(), pos.z()),
-                            yaw: transform.yaw(),
-                        },
-                    })
-                }
-                schemas::mmo::EventData::EntitySpawnEvent => {
-                    let fb_event = event
-                        .data_as_entity_spawn_event()
-                        .expect("event should be entity spawn event");
-                    let transform = fb_event.transform().expect("transform should be some");
-                    let pos = transform.position();
-                    events.push(GameEvent::SpawnEntity {
-                        entity_id: fb_event.entity_id(),
-                        transform: Transform {
-                            position: Vec3::new(pos.x(), pos.y(), pos.z()),
-                            yaw: transform.yaw(),
-                        },
-                    })
-                }
-                schemas::mmo::EventData::EntityDespawnEvent => {
-                    events.push(GameEvent::DespawnEntity {
-                        entity_id: event.data_as_entity_despawn_event().unwrap().entity_id(),
-                    })
-                }
-                event_type => {
-                    tracing::warn!(?event_type, "unhandled event type");
-                }
+    let Some(fb_events) = batch.events() else {
+        return Ok(());
+    };
+
+    for event in fb_events {
+        match event.data_type() {
+            schemas::mmo::EventData::mmo_ServerChatMessage => {
+                let fb_event = event
+                    .data_as_mmo_server_chat_message()
+                    .expect("event should be some");
+                events.push(GameEvent::Chat {
+                    channel: fb_event.channel(),
+                    author_name: fb_event
+                        .author_name()
+                        .expect("author should exist")
+                        .to_string(),
+                    text: fb_event.text().to_string(),
+                })
+            }
+            schemas::mmo::EventData::EntityMoveEvent => {
+                let fb_event = event
+                    .data_as_entity_move_event()
+                    .expect("event should be some");
+                let transform = fb_event.transform().expect("transform should be some");
+                let pos = transform.position();
+                events.push(GameEvent::MoveEntity {
+                    entity_id: fb_event.entity_id(),
+                    transform: Transform {
+                        position: Vec3::new(pos.x(), pos.y(), pos.z()),
+                        yaw: transform.yaw(),
+                    },
+                })
+            }
+            schemas::mmo::EventData::EntitySpawnEvent => {
+                let fb_event = event
+                    .data_as_entity_spawn_event()
+                    .expect("event should be entity spawn event");
+                let transform = fb_event.transform().expect("transform should be some");
+                let pos = transform.position();
+                events.push(GameEvent::SpawnEntity {
+                    entity_id: fb_event.entity_id(),
+                    transform: Transform {
+                        position: Vec3::new(pos.x(), pos.y(), pos.z()),
+                        yaw: transform.yaw(),
+                    },
+                })
+            }
+            schemas::mmo::EventData::EntityDespawnEvent => events.push(GameEvent::DespawnEntity {
+                entity_id: event.data_as_entity_despawn_event().unwrap().entity_id(),
+            }),
+            event_type => {
+                tracing::warn!(?event_type, "unhandled event type");
             }
         }
     }
