1517f968fa77888dd134795757e2cb35bad60c88
rejdeboer <rick.deboer@live.nl>
2025-07-28 15:26:15 +0200

    feat: directly send back connection handles

--- web-client/src/client.rs
+++ web-client/src/client.rs
@@ -1,6 +1,6 @@
 use futures_util::StreamExt;
 use http::{
-    Request,
+    Request, StatusCode,
     header::{self, HeaderValue},
 };
 use tokio::sync::{mpsc, watch};
@@ -10,47 +10,63 @@ use crate::{
     action::SocialAction, event::SocialEvent, reader::run_reader_task, writer::run_writer_task,
 };
 
-pub struct SocialClient {
-    pub command_tx: mpsc::Sender<SocialAction>,
-    pub event_rx: mpsc::Receiver<SocialEvent>,
+pub enum ConnectionError {
+    InvalidUrl,
+    InvalidTokenFormat,
+    // TODO: Send back error in json?
+    Http(StatusCode),
 }
 
-impl SocialClient {
-    pub async fn connect(
-        server_url: &str,
-        auth_token: &str,
-    ) -> Result<Self, Box<dyn std::error::Error>> {
-        let (command_tx, command_rx) = mpsc::channel::<SocialAction>(32);
-        let (event_tx, event_rx) = mpsc::channel::<SocialEvent>(32);
+pub async fn connect(
+    server_url: &str,
+    auth_token: &str,
+) -> Result<(mpsc::Sender<SocialAction>, mpsc::Receiver<SocialEvent>), ConnectionError> {
+    let request = create_connection_request(server_url, auth_token)?;
 
-        let bearer_token = format!("Bearer {auth_token}");
-        let auth_header = HeaderValue::from_str(&bearer_token)?;
+    let (ws_stream, _) =
+        tokio_tungstenite::connect_async(request)
+            .await
+            .map_err(|err| match err {
+                tokio_tungstenite::tungstenite::Error::Http(res) => {
+                    ConnectionError::Http(res.status())
+                }
+                err => unreachable!("encountered unreachable error: {:?}", err),
+            })?;
+    tracing::info!("WebSocket handshake with JWT auth successful");
 
-        let request = Request::builder()
-            .uri(server_url)
-            .header("Host", Url::parse(server_url)?.host_str().unwrap())
-            .header("Upgrade", "websocket")
-            .header("Connection", "Upgrade")
-            .header(
-                "Sec-WebSocket-Key",
-                tokio_tungstenite::tungstenite::handshake::client::generate_key(),
-            )
-            .header("Sec-WebSocket-Version", "13")
-            .header(header::AUTHORIZATION, auth_header)
-            .body(())?;
+    let (ws_writer, ws_reader) = ws_stream.split();
+    let (action_tx, action_rx) = mpsc::channel::<SocialAction>(32);
+    let (event_tx, event_rx) = mpsc::channel::<SocialEvent>(32);
+    let (shutdown_tx, shutdown_rx) = watch::channel(());
 
-        let (shutdown_tx, shutdown_rx) = watch::channel(());
+    tokio::spawn(run_writer_task(ws_writer, action_rx, shutdown_rx));
+    tokio::spawn(run_reader_task(ws_reader, event_tx, shutdown_tx));
 
-        let (ws_stream, _) = tokio_tungstenite::connect_async(request).await?;
-        tracing::info!("WebSocket handshake with JWT auth successful");
+    Ok((action_tx, event_rx))
+}
 
-        let (ws_writer, ws_reader) = ws_stream.split();
-        tokio::spawn(run_writer_task(ws_writer, command_rx, shutdown_rx));
-        tokio::spawn(run_reader_task(ws_reader, event_tx, shutdown_tx));
+fn create_connection_request(
+    server_url: &str,
+    auth_token: &str,
+) -> Result<Request<()>, ConnectionError> {
+    let bearer_token = format!("Bearer {auth_token}");
+    let auth_header =
+        HeaderValue::from_str(&bearer_token).map_err(|_| ConnectionError::InvalidTokenFormat)?;
 
-        Ok(Self {
-            command_tx,
-            event_rx,
-        })
-    }
+    let url = Url::parse(server_url).map_err(|_| ConnectionError::InvalidUrl)?;
+    let request = Request::builder()
+        .uri(server_url)
+        .header("Host", url.host_str().unwrap())
+        .header("Upgrade", "websocket")
+        .header("Connection", "Upgrade")
+        .header(
+            "Sec-WebSocket-Key",
+            tokio_tungstenite::tungstenite::handshake::client::generate_key(),
+        )
+        .header("Sec-WebSocket-Version", "13")
+        .header(header::AUTHORIZATION, auth_header)
+        .body(())
+        .map_err(|_| ConnectionError::InvalidTokenFormat)?;
+
+    Ok(request)
 }
--- web-client/src/lib.rs
+++ web-client/src/lib.rs
@@ -3,3 +3,7 @@ mod client;
 mod event;
 mod reader;
 mod writer;
+
+pub use action::SocialAction;
+pub use client::{ConnectionError, connect};
+pub use event::SocialEvent;
