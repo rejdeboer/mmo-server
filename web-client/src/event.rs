fef8d3b9eb7cb72f5c4f68c941cda92f8f0fd348
rejdeboer <rick.deboer@live.nl>
2025-07-29 08:58:11 +0200

    feat: also deserialize whispers

--- web-client/src/event.rs
+++ web-client/src/event.rs
@@ -11,6 +11,16 @@ pub enum SocialEvent {
         sender_name: String,
         sender_id: i32,
     },
+    Whisper {
+        text: String,
+        sender_name: String,
+        sender_id: i32,
+    },
+    WhisperReceipt {
+        text: String,
+        recipient_name: String,
+        recipient_id: i32,
+    },
 }
 
 impl SocialEvent {
@@ -25,6 +35,26 @@ impl SocialEvent {
                     channel: fb_event.channel(),
                     sender_name: fb_event.sender_name().to_string(),
                     sender_id: fb_event.sender_id(),
+                    text: fb_event.text().to_string(),
+                })
+            }
+            schema::EventData::ServerWhisper => {
+                let fb_event = event
+                    .data_as_server_whisper()
+                    .expect("event should be some");
+                Ok(Self::Whisper {
+                    sender_name: fb_event.sender_name().to_string(),
+                    sender_id: fb_event.sender_id(),
+                    text: fb_event.text().to_string(),
+                })
+            }
+            schema::EventData::ServerWhisperReceipt => {
+                let fb_event = event
+                    .data_as_server_whisper_receipt()
+                    .expect("event should be some");
+                Ok(Self::WhisperReceipt {
+                    recipient_name: fb_event.recipient_name().to_string(),
+                    recipient_id: fb_event.recipient_id(),
                     text: fb_event.text().to_string(),
                 })
             }
