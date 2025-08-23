ccb00fb4acec234ef7a62ea2da19c0cdbabf4779
rejdeboer <rick.deboer@live.nl>
2025-08-23 19:26:26 +0200

    fix: comment out connection guild id for now

--- mmo-server/src/systems/connection.rs
+++ mmo-server/src/systems/connection.rs
@@ -35,7 +35,7 @@ pub struct CharacterRow {
 pub enum EntityAttributes {
     Player {
         character_id: i32,
-        guild_id: Option<i32>,
+        // guild_id: Option<i32>,
     },
     Npc,
 }
@@ -48,7 +48,7 @@ impl EntityAttributes {
         match self {
             EntityAttributes::Player {
                 character_id,
-                guild_id: _,
+                // guild_id: _,
             } => {
                 let fb_attr = schema::PlayerAttributes::create(
                     builder,
