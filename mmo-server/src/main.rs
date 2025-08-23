7793ef3edda76ab22c9f7498aa0c3700e8ac0bb5
rejdeboer <rick.deboer@live.nl>
2025-08-23 17:04:26 +0200

    chore: remove unused custom logging for game server

--- mmo-server/src/main.rs
+++ mmo-server/src/main.rs
@@ -1,10 +1,6 @@
 use mmo_server::{application, configuration};
 
 fn main() -> std::io::Result<()> {
-    // TODO: Use custom logger?
-    // let subscriber = get_subscriber();
-    // subscriber.init();
-
     let settings = configuration::get_configuration().expect("config fetched");
 
     let (mut app, _) = application::build(settings)?;
