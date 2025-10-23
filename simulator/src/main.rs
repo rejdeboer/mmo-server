18430a344915d2cb058abf3b904da10866807609
rejdeboer <rick.deboer@live.nl>
2025-10-23 17:17:44 +0200

    feat: generate new seed

--- simulator/src/main.rs
+++ simulator/src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use rand::{Rng, SeedableRng};
+use rand_chacha::ChaCha8Rng;
 
 /// A CLI to simulate MMO traffic
 #[derive(Parser, Debug)]
@@ -6,12 +8,20 @@ use clap::Parser;
 struct Args {
     /// The simulation seed to use
     #[arg(short, long)]
-    seed: Option<String>,
+    seed: Option<u64>,
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
+
+    let seed = args.seed.unwrap_or_else(|| {
+        let random_seed = rand::rng().random();
+        println!("No seed provided, using randomly generated seed: {random_seed}");
+        random_seed
+    });
+
+    let mut rng = ChaCha8Rng::seed_from_u64(seed);
 
     Ok(())
 }
