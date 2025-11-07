32820393a040d06be3b691d0f027fe1f7fb5f757
rejdeboer <rick.deboer@live.nl>
2025-11-07 18:47:12 +0100

    fix: add bevy wayland feature to debug

--- mmo-server/src/plugins/debug.rs
+++ mmo-server/src/plugins/debug.rs
@@ -8,7 +8,7 @@ impl Plugin for AppPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(DefaultPlugins);
         app.add_plugins(ScheduleRunnerPlugin::run_loop(Duration::from_secs_f64(
-            Duration::from_secs_f64(crate::application::TICK_SECS),
+            crate::application::TICK_SECS,
         )));
         info!("running in debug mode");
     }
