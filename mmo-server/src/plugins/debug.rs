4ad6d51dbe3eed037e5b54458de8d81104763a99
rejdeboer <rick.deboer@live.nl>
2025-11-08 12:45:38 +0100

    feat: setup debug camera

--- mmo-server/src/plugins/debug.rs
+++ mmo-server/src/plugins/debug.rs
@@ -11,5 +11,15 @@ impl Plugin for AppPlugin {
             crate::application::TICK_SECS,
         )));
         info!("running in debug mode");
+
+        app.add_systems(Startup, setup_camera);
     }
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn((
+        Camera3d::default(),
+        Camera::default(),
+        Transform::from_xyz(-10., 10., 15.).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
 }
