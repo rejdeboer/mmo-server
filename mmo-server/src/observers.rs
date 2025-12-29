6225ca83836fb82b0163736bf39d9936513e94d5
rejdeboer <rick.deboer@live.nl>
2025-12-29 21:33:00 +0100

    feat: remove living bundle upon entity death

--- mmo-server/src/observers.rs
+++ mmo-server/src/observers.rs
@@ -1,4 +1,4 @@
-use crate::components::Dead;
+use crate::components::{Dead, MovementSpeedComponent, Vitals};
 use bevy::prelude::*;
 use std::time::Duration;
 
@@ -7,8 +7,19 @@ const CORPSE_DESPAWN_DURATION: Duration = Duration::from_secs(150);
 #[derive(EntityEvent)]
 pub struct EntityDeath(pub Entity);
 
+#[derive(Bundle)]
+/// Components that living world actors have
+struct LivingBundle {
+    vitals: Vitals,
+    movement_speed: MovementSpeedComponent,
+}
+
 pub fn on_entity_death(event: On<EntityDeath>, mut commands: Commands) {
-    commands.entity(event.0).insert(Dead {
-        despawn_timer: Timer::new(CORPSE_DESPAWN_DURATION, TimerMode::Once),
-    });
+    let entity = event.0;
+    commands
+        .entity(entity)
+        .remove::<LivingBundle>()
+        .insert(Dead {
+            despawn_timer: Timer::new(CORPSE_DESPAWN_DURATION, TimerMode::Once),
+        });
 }
