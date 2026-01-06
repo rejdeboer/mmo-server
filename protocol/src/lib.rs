726772e9d4e4b1cd56aa5a21606f81ec6a5899ff
rejdeboer <rick.deboer@live.nl>
2026-01-06 10:14:30 +0100

    feat: define remaining protocol types

--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -31,6 +31,12 @@ pub struct Vitals {
 }
 
 #[derive(Encode, Decode)]
+pub struct ItemDrop {
+    item_id: u32,
+    quantity: u16,
+}
+
+#[derive(Encode, Decode)]
 pub struct Actor {
     /// The entity ID assigned by bevy
     id: u64,
@@ -44,13 +50,42 @@ pub struct Actor {
 
 #[derive(Encode, Decode)]
 pub enum ServerEvent {
-    ActorMoveEvent {
+    ActorMove {
         entity_id: u64,
         position: Vec3,
         yaw: u16,
     },
-    ActorSpawnEvent(Box<Actor>),
-    ActorDespawnEvent(u64),
+    ActorSpawn(Box<Actor>),
+    ActorDespawn(u64),
+    ActorDeath(u64),
+    KillReward {
+        victim_id: u64,
+        loot: Vec<ItemDrop>,
+    },
+    StartCasting {
+        actor_id: u64,
+        spell_id: u32,
+    },
+    SpellImpact {
+        // TODO: How will we do spells that do not have a target, or do not deal damage?
+        // We probably want to implement impact type, like damage, heal, etc...
+        target_id: u64,
+        spell_id: u32,
+        impact_amount: i32,
+    },
+    Chat {
+        channel: ChatChannel,
+        sender_name: String,
+        text: String,
+    },
+}
+
+#[repr(u8)]
+#[derive(Encode, Decode)]
+pub enum ChatChannel {
+    Say,
+    Yell,
+    Zone,
 }
 
 #[derive(Encode, Decode)]
@@ -65,4 +100,13 @@ pub enum PlayerAction {
         spell_id: u32,
         target_entity_id: u64,
     },
+    Chat {
+        channel: ChatChannel,
+        text: String,
+    },
+}
+
+#[derive(Encode, Decode)]
+pub struct EnterGameResponse {
+    player_actor: Actor,
 }
