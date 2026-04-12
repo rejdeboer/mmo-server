d54c3c317749edf16fc72e6b6f76690db0d9b366
Rick de Boer <rick.deboer@live.nl>
2026-04-12 15:15:32 +0200

    fix: default rustls provider

--- crates/web-server/src/main.rs
+++ crates/web-server/src/main.rs
@@ -9,6 +9,10 @@ async fn main() -> std::io::Result<()> {
     let settings = configuration::get_configuration().expect("config fetched");
     init_telemetry(&settings.telemetry);
 
+    rustls::crypto::ring::default_provider()
+        .install_default()
+        .expect("failed to install rusls crypto provider");
+
     let application = Application::build(settings).await?;
     application.run_until_stopped().await?;
     Ok(())
